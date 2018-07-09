85bc2f71b4238c539e0f6c1b134a37952f782ceb
Artur Lukin <lukin.arthur@yandex.ru>
2018-07-09 15:00:25 +0300

    Added: ICO storage

--- contracts/BREMFactory.sol
+++ contracts/BREMFactory.sol
@@ -5,6 +5,13 @@ import "./BREMICO.sol";
 
 contract BREMFactory {
     
+    address[] public projects;
+    mapping(string => uint256) indexes;
+    
+    constructor() public {
+        projects.push(address(0));
+    }
+    
     function createBREMICO(
         string _name, 
         string _symbol,
@@ -13,12 +20,15 @@ contract BREMFactory {
     public
     returns (address tokenAddress, address icoAddress)
     {
-        // TODO: Checking for name and _symbol
+        require(bytes(_name).length > 0 && bytes(_symbol).length > 0);
+        require(indexes[_name] == 0);
         
         BREMToken token = new BREMToken(_name, _symbol);
         tokenAddress = address(token);
         
         BREMICO ico = new BREMICO(_rate, msg.sender, token);
         icoAddress = address(ico);
+        projects.push(icoAddress);
+        indexes[_name] = projects.length - 1;
     }
 }
