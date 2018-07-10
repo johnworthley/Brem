bf4f7513595d1e7a7fe4987785071e9c2761a723
Artur Lukin <lukin.arthur@yandex.ru>
2018-07-10 15:33:37 +0300

    Fixed: remover factory fucntion from factory contract

--- contracts/BREMFactory.sol
+++ contracts/BREMFactory.sol
@@ -18,29 +18,4 @@ contract BREMFactory {
         address indexed tokenAddress,
         string name
     );
-    
-    function createBREMICO( 
-        string _name, 
-        string _symbol,
-        uint256 _rate,
-        uint256 _totalStages,
-        string _description,
-        bytes32[] _docHashes
-    ) 
-    public
-    returns (address tokenAddress, address icoAddress)
-    {
-        require(bytes(_name).length > 0 && bytes(_symbol).length > 0);
-        require(indexes[_name] == 0);
-        
-        BREMToken token = new BREMToken(_name, _symbol);
-        tokenAddress = address(token);
-        
-        BREMICO ico = new BREMICO(_rate, msg.sender, token, _totalStages,
-            _description, _docHashes);
-        icoAddress = address(ico);
-        projects.push(icoAddress);
-        indexes[_name] = projects.length - 1;
-        emit BREMICOCreated(msg.sender, icoAddress, tokenAddress, _name);
-    }
 }
