85e45fe0a3786a4f87a8e0413138af724c12601f
Artur Lukin <lukin.arthur@yandex.ru>
2018-07-11 15:39:32 +0300

    Added: BREM simple test Removed: truffle template test

---
+++ test/BREM.test.js
@@ -0,0 +1,14 @@
+const BREM = artifacts.require('BREM');
+
+contract('BREM', function(accounts) {
+
+    let brem;
+
+    before(async function() {
+        brem = await BREM.new();
+    });
+
+    it('Check superuser address', async function() {
+        console.log(accounts);
+    });
+});
