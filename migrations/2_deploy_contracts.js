feef7b6a0b7c4dd2afb6c875beb1fe1a793a4dde
Artur Lukin <lukin.arthur@yandex.ru>
2018-07-20 02:17:13 +0300

    Fixed: contracts deploying

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,6 +3,8 @@ var BRM = artifacts.require("./BRMToken.sol");
 
 module.exports = function(deployer) {
     deployer.deploy(BRM).then(function() {
+        deployer.link(BRM, BREM);
+    }).then(function() {
         deployer.deploy(BREM, BRM.address);
-    })
+    });
 };
