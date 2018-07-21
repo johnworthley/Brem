75ec844503a862694f22da24290bdbab4a52387a
Artur Lukin <lukin.arthur@yandex.ru>
2018-07-21 18:46:36 +0300

    Added: Superuser, Auditor and Developer start account forms

--- src/user/ui/profileform/ProfileForm.js
+++ src/user/ui/profileform/ProfileForm.js
@@ -6,50 +6,52 @@ const contract = require('truffle-contract');
 
 class ProfileForm extends Component {
 
-    constructor(props) {
-        super(props)
+constructor(props) {
+    super(props)
 
-        this.state = {
-            BREMInstance: null,
-            name: this.props.name,
-            address: null,
-        }
-
-        this.init();
+    this.state = {
+        BREMInstance: null,
+        name: this.props.name,
+        address: null,
     }
 
-    init() {
-        const web3 = store.getState().web3.web3Instance;
-        if (typeof web3 !== 'undefined') {
-            const brem = contract(BREMContract);
-            brem.setProvider(web3.currentProvider);
-            brem.deployed().then((instance) => {
-                this.setState({BREMInstance: instance});
-                // Get coinbase
-                web3.eth.getCoinbase((err, coinbase) => {
-                    if (err) {
-                        console.error(err);
+    this.init();
+}
+
+init() {
+    const web3 = store.getState().web3.web3Instance;
+    if (typeof web3 !== 'undefined') {
+        const brem = contract(BREMContract);
+        brem.setProvider(web3.currentProvider);
+        brem.deployed().then((instance) => {
+            this.setState({BREMInstance: instance});
+            // Get coinbase
+            web3.eth.getCoinbase((err, coinbase) => {
+                if (err) {
+                    console.error(err);
+                }
+                this.setState({address: coinbase});
+                instance.isDeveloper(coinbase).then((res) => {
+                    if (res) {
+                        this.setState({ role: 'developer' })
                     }
-                    this.setState({address: coinbase});
-                    instance.isDeveloper(coinbase).then((res) => {
-                        if (res) {
-                            this.setState({ role: 'developer' })
-                        }
-                    })
-                    instance.isAuditor(coinbase).then((res) => {
-                        if (res) {
-                            this.setState({ role: 'auditor' })
-                        }
-                    })
-                    instance.isSuperuser(coinbase).then((res) => {
+                })
+                instance.isAuditor(coinbase).then((res) => {
+                    if (res) {
+                        this.setState({ role: 'auditor' })
+                    }
+                })
+                instance.isSuperuser(coinbase).then((res) => {
+                    if (res) {
                         this.setState({ role: 'superuser'})
-                    })
+                    }
                 })
             })
-        } else {
-            console.error('Web3 is not initialized.');
-        }
+        })
+    } else {
+        console.error('Web3 is not initialized.');
     }
+}
 
   handleSubmit(event) {
     event.preventDefault()
@@ -80,9 +82,17 @@ class ProfileForm extends Component {
                     <button type="submit" className="pure-button pure-button-primary">Update</button>
                 </fieldset>
             </form>
+            
+            { this.state && this.state.role && this.state.role === 'superuser' &&
+                SuperuserForm
+            }
 
-            { this.state && this.state.role &&
-                <h1>TODO: Switch by account type</h1>
+            { this.state && this.state.role && this.state.role === 'developer' &&
+                DeveloperForm
+            }
+
+            { this.state && this.state.role && this.state.role === 'auditor' &&
+                AuditorForm
             }
             </div>
     )
