96fe97e4b64de3e0a95777e022d815c797ea05c8
Artur Lukin <lukin.arthur@yandex.ru>
2018-08-12 12:19:20 +0300

    Updated: tables

--- server/data/setup.sql
+++ server/data/setup.sql
@@ -5,13 +5,15 @@ DROP TABLE icoAuditors CASCADE;
 DROP TYPE status;
 
 CREATE TABLE developers (
-  id      SERIAL PRIMARY KEY,
-  address VARCHAR(42) UNIQUE NOT NULL
+  id        SERIAL PRIMARY KEY,
+  address   VARCHAR(42) UNIQUE NOT NULL,
+  username  VARCHAR(64) UNIQUE NOT NULL
 );
 
 CREATE TABLE auditors (
-  id      SERIAL PRIMARY KEY,
-  address VARCHAR(42) UNIQUE NOT NULL
+  id        SERIAL PRIMARY KEY,
+  address   VARCHAR(42) UNIQUE NOT NULL,
+  username  VARCHAR(64) UNIQUE NOT NULL
 );
 
 CREATE TYPE status AS ENUM (
@@ -20,14 +22,22 @@ CREATE TYPE status AS ENUM (
   'success',
   'failed',
   'requested',
-  'withdrawn'
+  'withdrawn',
+  'overdue'
 );
 
 CREATE TABLE  ico (
-  id          SERIAL PRIMARY KEY,
-  address     VARCHAR(42) UNIQUE NOT NULL,
-  developerID INT REFERENCES developers(id),
-  status      status
+  id            SERIAL PRIMARY KEY,
+  address       VARCHAR(42) UNIQUE NOT NULL,
+  developerID   INT REFERENCES developers(id),
+  cap           INT NOT NULL,
+  description   TEXT,
+  closingTime   TIMESTAMP NOT NULL,
+  feePercent    INT NOT NULL,
+  tokenAddress  VARCHAR(42) UNIQUE NOT NULL,
+  name          VARCHAR(64) UNIQUE NOT NULL,
+  symbol        VARCHAR(7) NOT NULL,
+  status        status
 );
 
 CREATE TABLE icoAuditors (
