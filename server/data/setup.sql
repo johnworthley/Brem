9fb59ab74fb685421136caaf7d7c5002e8e3e611
Artur Lukin <lukin.arthur@yandex.ru>
2018-08-14 01:37:13 +0300

    Updated: tables

--- server/data/setup.sql
+++ server/data/setup.sql
@@ -1,8 +1,14 @@
+DROP TABLE  superuser CASCADE;
 DROP TABLE developers CASCADE;
 DROP TABLE auditors CASCADE;
 DROP TABLE ico CASCADE;
 DROP TABLE icoAuditors CASCADE;
 DROP TYPE status;
+
+CREATE TABLE superuser (
+  id      SERIAL PRIMARY KEY,
+  address VARCHAR(42) UNIQUE NOT NULL
+);
 
 CREATE TABLE developers (
   id        SERIAL PRIMARY KEY,
@@ -13,7 +19,7 @@ CREATE TABLE developers (
 CREATE TABLE auditors (
   id        SERIAL PRIMARY KEY,
   address   VARCHAR(42) UNIQUE NOT NULL,
-  username  VARCHAR(64) UNIQUE NOT NULL
+  username  VARCHAR(64)
 );
 
 CREATE TYPE status AS ENUM (
@@ -30,14 +36,15 @@ CREATE TABLE  ico (
   id            SERIAL PRIMARY KEY,
   address       VARCHAR(42) UNIQUE NOT NULL,
   developerID   INT REFERENCES developers(id),
-  cap           INT NOT NULL,
   description   TEXT,
   closingTime   TIMESTAMP NOT NULL,
   feePercent    INT NOT NULL,
   tokenAddress  VARCHAR(42) UNIQUE NOT NULL,
-  name          VARCHAR(64) UNIQUE NOT NULL,
+  name          VARCHAR(256) UNIQUE NOT NULL,
   symbol        VARCHAR(7) NOT NULL,
-  status        status
+  status        status,
+  location      TEXT,
+  locAddress    TEXT
 );
 
 CREATE TABLE icoAuditors (
