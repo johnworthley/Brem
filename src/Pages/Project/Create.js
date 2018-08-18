d66c09c861375712268765c5884d65856e22daec
forgetable <io@forgetable.ru>
2018-08-18 18:38:38 +0300

    Project templates

---
+++ src/Pages/Project/Create.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Create extends Component {
+  state = {
+
+  }
+
+  render() {
+
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Create
---
+++ src/Pages/Project/List.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class List extends Component {
+  state = {
+
+  }
+
+  render() {
+
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default List
