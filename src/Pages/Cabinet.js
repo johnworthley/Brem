e5cd6b1c0a83471558ab655f0511fcd031f536c1
Artur Lukin <lukin.arthur@yandex.ru>
2018-08-19 16:35:26 +0300

    Added: change fee and add audotr functions

--- src/Pages/Cabinet.js
+++ src/Pages/Cabinet.js
@@ -3,6 +3,11 @@ import { css, StyleSheet } from 'aphrodite/no-important'
 
 import Table from '../Components/Table'
 
+import getWebThree from '../util/getweb3'
+import store from 'Store'
+import contract from 'truffle-contract'
+import BREMContract from "../../build/contracts/BREM.json"
+
 const style = StyleSheet.create({
 	main: {
 		marginBottom: 75
@@ -27,15 +32,92 @@ export default class Cabinet extends Component {
 		}
 	}
 
-	changeFee = e => {
+	changeFee = async e => {
 		e.preventDefault()
 		const fee = this.fee.value
-		console.log(fee)
+    
+    await getWebThree()
+    const { web3Instance, web3Account } = store
+    const { currentProvider, utils, eth } = web3Instance
+    const brem = contract(BREMContract)
+    brem.setProvider(currentProvider)
+
+    const coinbase = await eth.getCoinbase()
+    // TODO: check coinbase with state
+
+    // Check for contract's current fee value
+    const currentFee = await eth.getBalance(brem.address)
+    if (currentFee === fee) {
+      // Значение уже установлено
+      return
+    }
+
+    const bremInstance = await brem.deployed()
+    const isSuperuser = await bremInstance.isSuperuser(coinbase)
+    if (!isSuperuser) {
+      // Ошибка, пользователь не админ
+      return
+    }
+    // Change fee
+    try {
+      const txRes = await bremInstance.setWithdrawFeePercent(fee, {from: coinbase})
+      const tx = txRes.tx
+      const status = txRes.receipt.status
+      if (status === "0x1") {
+        alert(tx)
+        // Можно обновить текущее значение из web3
+      } else{
+        alert("Error tx")
+      }
+    } catch(err) {
+      console.error(err)
+    }
 	}
-	addAuditors = e => {
+	addAuditors = async e => {
 		e.preventDefault()
-		const auditors = this.auditors.value
-		console.log(auditors)
+    const auditor = this.auditors.value
+    
+    await getWebThree()
+    const { web3Instance, web3Account } = store
+    const { currentProvider, utils, eth } = web3Instance
+
+    if (!utils.isAddress(auditor)) {
+      return alert("Error")
+    }
+
+    const brem = contract(BREMContract)
+    brem.setProvider(currentProvider)
+
+    const coinbase = await eth.getCoinbase()
+    // TODO: check coinbase with state
+
+    const bremInstance = await brem.deployed()
+    const isSuperuser = await bremInstance.isSuperuser(coinbase)
+    if (!isSuperuser) {
+      // Ошибка, пользователь не админ
+      return
+    }
+
+    // Check for auditor
+    const isAuditor = bremInstance.isAuditor(auditor)
+    if (isAuditor) {
+      // Уже аудитор
+      return
+    }
+    // Add audtor
+    try {
+      const txRes = await bremInstance.addAutors(auditor, {from: coinbase})
+      const tx = txRes.tx
+      const status = txRes.receipt.status
+      if (status === "0x1") {
+        alert(tx)
+        // Сообщение, что аудитор появиться в списке после регистрации
+      } else{
+        alert("Error tx") // Можно добавить ссылку на etherscan
+      }
+    } catch(err) {
+      console.error(err)
+    }
 	}
 
 	render = () => {
