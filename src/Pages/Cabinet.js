06c9f2acb271d9f2e1ec014214e0393078b2b599
forgetable <io@forgetable.ru>
2018-08-19 04:40:39 +0300

    Form placeholders

--- src/Pages/Cabinet.js
+++ src/Pages/Cabinet.js
@@ -3,6 +3,12 @@ import { css, StyleSheet } from 'aphrodite/no-important'
 
 import Table from '../Components/Table'
 
+const style = StyleSheet.create({
+	main: {
+		marginBottom: 75
+	}
+})
+
 export default class Cabinet extends Component {
 	state = {
 		struct: {
@@ -20,13 +26,35 @@ export default class Cabinet extends Component {
 			])
 		}
 	}
+
+	changeFee = e => {
+		e.preventDefault()
+		const fee = this.fee.value
+		console.log(fee)
+	}
+	addAuditors = e => {
+		e.preventDefault()
+		const auditors = this.auditors.value
+		console.log(auditors)
+	}
+
 	render = () => {
 		//const { struct } = this.props
 		const { struct } = this.state
 		return (
-			<div>
-				Cabinet
-				
+			<div className={css(style.main)}>
+				<h3>Cabinet</h3>
+				<div style={{marginBottom: 100}}>
+					<form onSubmit={this.changeFee} style={{marginBottom: 10}}>
+						<input required ref={elem => this.fee = elem} type="text" placeholder="Fee" />
+						<button>Change fee</button>
+					</form>
+					<form onSubmit={this.addAuditors}>
+						<input required ref={elem => this.auditors = elem} type="text" placeholder="Auditors" />
+						<button>Add auditors</button>
+					</form>
+				</div>
+
 				<Table struct={struct} />
 			</div>
 		)
